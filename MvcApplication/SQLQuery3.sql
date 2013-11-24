0388569c215db292b4e6b86211ac80ed59c391d1
Vanya Skorodumov <iskorod23@gmail.com>
2013-11-24 19:58:34 +0400

    all

---
+++ MvcApplication/SQLQuery3.sql
@@ -0,0 +1,11 @@
+﻿insert into [dbo].[User]
+(id, email, password)
+values
+(3, N'dndfjxj', '23'),
+(4, N'dmsk', '21'),
+(5, N'sja', '12'),
+(6, N'sjaa', '212'),
+(7, N'asdjks', '1211212312'),
+(8, N'NAJDNJADSBNJ', 's1wdjdk'),
+(9, N'djsaijskjnksnknsknkn', 'skokqojkdojekoj'),
+(10, N'sq@eer4', 'dkolkdodeo133')
