923a28c8711a59232d07285a39e8ab273673c165
Vanya Skorodumov <iskorod23@gmail.com>
2013-11-24 21:06:13 +0400

    kdd

---
+++ MvcApplication/MvcApplication1/Models/UserModel.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace MvcApplication1.Models
+{
+    public class UserModel
+    {
+        private string connStr = "";
+        public string[] GetUserNames()
+        {
+
+            SqlConnection myConn = new SqlConnection(connStr);
+            myConn.Open();
+
+            SqlCommand command = myConn.CreateCommand();
+            command.CommandText = "SELECT email from User";
+               
+            
+           SqlDataReader reader = command.ExecuteReader();
+            List<String> emails = new List<string>();
+            while (reader.Read())
+            {
+
+                emails.Add(reader["email"].ToString());
+
+            }
+
+            return emails.ToArray();
+          
+        }
+    }
+}
